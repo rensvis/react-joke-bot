7ed5ede9eff4d478e8fe708182dd894247c33d5c
rensvis <joshuarensvis@gmail.com>
2021-05-06 11:21:44 +0200

    fix - minor styling updates

--- src/App.js
+++ src/App.js
@@ -8,16 +8,22 @@ function App() {
 
   const toggleChatVisibility = () => {
     setIsChatExpanded(!isChatExpanded);
+    const body = document.getElementsByTagName("body")[0];
+    if (isChatExpanded) {
+      body.classList.remove("no-scroll"); // allow scroll on body
+    } else {
+      body.classList.add("no-scroll"); // prevent scroll on body
+    }
   };
 
   return (
     <div className="App">
       <main className="App-main">
         <div className="container">
-          <h1>
-            React Joke Bot
-          </h1>
-          <h3>A project by <a href="http://rensvis.nl/">Rens Vis</a></h3>
+          <h1>React Joke Bot</h1>
+          <h3>
+            A project by <a href="http://rensvis.nl/">Rens Vis</a>
+          </h3>
           <p>
             Welcome to the React Joke Bot. Press the button in the bottom right
             of the page to have a laugh!
@@ -33,7 +39,9 @@ function App() {
 
           <h4>Under the hood</h4>
           <p>
-            This is (clearly) a <i className="fab fa-react react-logo"></i> <span className="bold">React</span> project. The project consists of the following different components:
+            This is (clearly) a <i className="fab fa-react react-logo"></i>{" "}
+            <span className="bold">React</span> project. The project consists of
+            the following different components:
           </p>
           <ul>
             <li>ModalOverlay (dark background when the chatbox opens)</li>
@@ -44,15 +52,24 @@ function App() {
             <li>ChatNewMessage (bottom section in the chatbox)</li>
           </ul>
 
-          <p>The jokes are pulled in via this <span className="bold">api</span>: <a href="https://jokeapi.dev/">joke api</a>.</p>
+          <p>
+            The jokes are pulled in via this <span className="bold">api</span>:{" "}
+            <a href="https://jokeapi.dev/">joke api</a>.
+          </p>
 
           <p>
-            Check out the full <span className="bold">sourcecode</span> on <a href="https://github.com/rensvis/react-joke-bot"><i className="fab fa-github"></i> GitHub</a>.
+            Check out the full <span className="bold">sourcecode</span> on{" "}
+            <a href="https://github.com/rensvis/react-joke-bot">
+              <i className="fab fa-github"></i> GitHub
+            </a>
+            .
           </p>
         </div>
 
         <div className="container footer">
-          <p>- 2021 <a href="http://rensvis.nl/">Rens Vis</a> -</p>
+          <p>
+            - 2021 <a href="http://rensvis.nl/">Rens Vis</a> -
+          </p>
         </div>
       </main>
       <ModalOverlay
